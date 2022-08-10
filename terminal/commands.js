aeb4b025f2853572534a6f4dc80d0ac29f530d27
Anas Yunus <anasyunusind@gmail.com>
2022-08-10 23:32:20 +0530

    First Commit

---
+++ terminal/commands.js
@@ -0,0 +1,53 @@
+banner = [
+    "<span style='color: var(--green);'>                    __																	</span>",
+    '<span style="color: var(--green);">        ..======.. |==|        	    </span><span style="color: var(--red);">___                         __  __                        </span>',
+    '<span style="color: var(--green);">        || <span style=\"color: var(--red);\">C>_</span>  || |= |        	   </span><span style="color: var(--red);">/   |   ____   ____ _ _____  \\ \\/ /__  __ ____   __  __ _____</span>',
+    "<span style='color: var(--green);'>     _  ||      || |^<span style='color: var(--red);'>*</span>| _      	  </span><span style='color: var(--red);'>/ /| |  / __ \\ / __ `// ___/   \\  // / / // __ \\ / / / // ___/</span>",
+    "<span style='color: var(--green);'>    |o| o=,====,=o |__||o|     	 </span><span style='color: var(--red);'>/ ___ | / / / // /_/ /(__  )    / // /_/ // / / // /_/ /(__  )</span>",
+    "<span style='color: var(--green);'>    |_|  _______)   )  |_|     	</span><span style='color: var(--red);'>/_/  |_|/_/ /_/ \\__,_//____/    /_/ \\__,_//_/ /_/ \\__,_//____/<i>.dev</i></span>",
+    "<span style='color: var(--green);'>        [=======]  ()                                                                      <span style=\"color: var(--yellow); font-style: italic;\">© 2022</span></span>",
+    "<span style='color: var(--green);'>====================================================================================================></span>",
+    '<span style="color: var(--blue);">>></span> Welcome to My Interactive Web Terminal',
+    '<span style="color: var(--blue);">>></span> Type <span style="color: var(--red);">help</span> to get list of commands</p>',
+    ' '
+    ]
+
+commands = [
+    '   <span style="color: var(--red);">about</span>       About Me and Myself',
+    '   <span style="color: var(--red);">projects</span>    My past, present & prospective Projects',
+    '   <span style="color: var(--red);">contact</span>     My Socials and Contact Details',
+    '   <span style="color: var(--red);">banner</span>      Print the ASCII Banner',
+    '   <span style="color: var(--red);">help</span>        I think you know what this is',
+    '   <span style="color: var(--red);">norm</span>        Takes you to my Non-Cool Normal Website',
+    ' '
+]
+
+about = [
+    '   Hi, My name is Anas Yunus',
+    '   I\'m a 17y/o from Kerala, India',
+    '   Currently a CSE fresher at SRM Chennai',
+    '   Passionate Coder, Lazy piece of shit, and a virgin(obviously)',
+    ' ',
+]
+
+projects = [
+    '   Uhhhh......',
+    '   Coming Soon..!!!!',
+    '   ig 🤷🏻‍♂️',
+    '    '
+]
+
+contact = [
+    '   <span style="color: var(--blue);">-g</span>  <span style="color: var(--red);">github</span>     <span style="color: var(--white);">@anas-yunus</span>',
+    '   <span style="color: var(--blue);">-t</span>  <span style="color: var(--red);">twitter</span>    <span style="color: var(--white);">@anasyunus_</span>',
+    '   <span style="color: var(--blue);">-i</span>  <span style="color: var(--red);">instagram</span>  <span style="color: var(--white);">@anasyunus_</span>',
+    '   <span style="color: var(--blue);">-e</span>  <span style="color: var(--red);">email</span>      <span style="color: var(--white);">anasyunusind@gmail.com</span>',
+    ' '
+]
+
+instagram = ['  Opening on instagram.com...',' ']
+github = ['  Opening on github.com...',' ']
+twitter = ['  Opening on twitter.com...',' ']
+email = ['  Preparing to send email...',' ']
+
+invalid = ["Invalid Command, Use <span style='color: var(--red);'>help</span> to get list of commands",' ']
---
+++ terminal/terminal-app.js
@@ -0,0 +1,129 @@
+const inputEl = document.getElementById("input-box")
+const printArea = document.getElementById("print-area")
+const printBOX = document.getElementById("print-box")
+const commandDivEl = document.getElementById("command-div")
+let txt = '<p><span style="color: var(--blue);">>></span> Type <a href="#" style=" color: var(--red);">norm</a> for Normal Website</p>'
+addLine(txt, '', 0)
+
+loopLines(banner, "", 100);
+inputEl.focus()
+
+document.addEventListener('keypress', (event) => {
+    inputEl.focus()
+    if (event.key === "Enter" && inputEl.value !== "") {
+
+        let cmdValue = inputEl.value.toLowerCase();
+
+        addLine(`<span style="color: var(--blue);">>></span> <span style="color: var(--yellow);">${cmdValue}</span>`, "commands", 0)
+
+
+        switch (cmdValue) {
+            case "banner":
+            case "-b":
+                loopLines(banner, "", 100);
+                break;
+            case "help":
+            case "-h":
+                loopLines(commands, "", 100);
+                break;
+            case "about":
+            case "-a":
+                loopLines(about, "", 100);
+                break;
+            case "projects":
+            case "-p":
+                loopLines(projects, "", 100);
+                break;
+            case "contact":
+            case "-c":
+                loopLines(contact, "", 100);
+                break;
+            case "norm":
+            case "-n":
+                loopLines(commands, "", 100);
+                break;
+            case "github":
+            case "-g":
+                loopLines(github, "", 100);
+                setTimeout(() => {
+                    window.open(
+                        "https://github.com/anas-yunus", "_blank");
+                }, 1000);
+                break;
+            case "instagram":
+            case "-i":
+                loopLines(instagram, "", 100);
+                setTimeout(() => {
+                    window.open(
+                        "https://www.instagram.com/instagram", "_blank");
+                }, 1000);
+                break;
+            case "twitter":
+            case "-t":
+                loopLines(twitter, "", 100);
+                setTimeout(() => {
+                    window.open(
+                        "https://www.twitter.com/anasyunus_", "_blank");
+                }, 1000);
+                break;
+            case "email":
+            case "-e":
+                loopLines(email, "", 100);
+                setTimeout(() => {
+                    window.open(
+                        "mailto:anasyunusind@gmail.com", "_blank");
+                }, 1000);
+                break;
+            case "sudo$admin":
+                addLine("you are now admin", "", 100);
+                break;
+
+
+            default:
+                loopLines(invalid, "", 100);
+                break;
+        }
+
+        inputEl.value = ""
+        inputEl.style.width = "0ch"
+        inputEl.focus()
+    }
+})
+
+
+function addLine(text, style, time) {
+    var t = "";
+    for (let i = 0; i < text.length; i++) {
+        if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+            t += "&nbsp;&nbsp;";
+            i++;
+        } else {
+            t += text.charAt(i);
+        }
+    }
+    setTimeout(function () {
+        var next = document.createElement("pre");
+        next.innerHTML = t;
+        next.className = style;
+
+        commandDivEl.parentNode.insertBefore(next, commandDivEl);
+
+        window.scrollTo(0, document.body.offsetHeight);
+    }, time);
+}
+
+function loopLines(name, style, time) {
+    name.forEach(function (item, index) {
+        addLine(item, style, index * time);
+    });
+}
+
+
+inputEl.addEventListener('input', resizeInput); // bind the "resizeInput" callback on "input" event
+resizeInput.call(inputEl); // immediately call the function
+
+function resizeInput() {
+    this.style.width = this.value.length + "ch";
+}
+
+
